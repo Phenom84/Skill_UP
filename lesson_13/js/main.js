03a60c7a1c5fca81c4849fc45569450e13a295e6
Cerneaga Denis <prolevelmd@gmail.com>
2020-03-17 11:31:52 +0200

    Code improvement for lesson 13

--- lesson_13/js/main.js
+++ lesson_13/js/main.js
@@ -1,29 +1,33 @@
+function checkExistDomElement(element) {
+    for (let i = 0; i < element.length; i++) {
+        if (!document.getElementById(element[i])) {
+            console.log("Element with ID '" + element[i] + "' not found");
+            return false;
+        }
+    }
+    return true;
+}
+
 function calculation() {
+    let elementArray = ['coefficient_a', 'coefficient_b', 'coefficient_c', 'result_D', 'result_case', 'result'];
+    if (!checkExistDomElement(elementArray)) {
+        return;
+    }
     let a = document.getElementById("coefficient_a").value;
     let b = document.getElementById("coefficient_b").value;
     let c = document.getElementById("coefficient_c").value;
     let D = discriminant(a, b, c);
-
     document.getElementById("result_D").innerHTML = D;
-    switch (true) {
-        case isNaN(a) || isNaN(b) || isNaN(c) == true:
-            alert("Вводить можно только числа!");
-            location.reload();
-            break;
-        case D < 0:
-            document.getElementById("result_case").innerHTML = "Нет действительных корней.";
-            showResult();
-            break;
-        case D == 0:
-            document.getElementById("result_case").innerHTML = "Один действительный корень: " + (-b / (2 * a));
-            showResult();
-            break;
-        default:
-            document.getElementById("result_case").innerHTML =
-                "Два действительных корня: <br><span class='formula'>x<sub>1</sub></span> =  " + (-b - Math.sqrt(D)) / (2 * a) +
-                "<br><span class='formula'>x<sub>2</sub></span> =  " + (-b + Math.sqrt(D)) / (2 * a);
-            showResult();
-            break;
+    if (isNaN(a) || isNaN(b) || isNaN(c) == true){
+        alert("Вводить можно только числа!");
+        location.reload(); 
+    }else if (D < 0){
+        showResult("Нет действительных корней.");
+    }else if (D == 0){
+        showResult("Нет действительных корней.");   
+    }else {
+        showResult("Два действительных корня: <br><span class='formula'>x<sub>1</sub></span> =  " + (-b - Math.sqrt(D)) / (2 * a) +
+                "<br><span class='formula'>x<sub>2</sub></span> =  " + (-b + Math.sqrt(D)) / (2 * a));
     }
 }
 
@@ -31,6 +35,7 @@ function discriminant(a, b, c) {
     return b * b - 4 * a * c;
 }
 
-function showResult() {
+function showResult(result) {
+    document.getElementById("result_case").innerHTML = result;
     document.getElementById("result").style.display = "block";
 }
