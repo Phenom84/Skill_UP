4e71eb6c857196b43701fa252bac77cdbcad6a7e
Cerneaga Denis <prolevelmd@gmail.com>
2020-03-21 01:21:00 +0200

    Add homework for lesson 15

---
+++ lesson_15/js/main.js
@@ -0,0 +1,7 @@
+function showResult(string) {
+    if (document.getElementById("resultSting")) {
+        document.getElementById("resultSting").innerHTML = string;
+    }
+}
+secondButton.onclick = function () { showResult('You clicked Second button') };
+thirdButton.addEventListener("click", function () { showResult('You clicked Third button') });
