e187adb9997c1acaf5fcdb1c01ec4d45064e4f28
Cerneaga Denis <prolevelmd@gmail.com>
2020-03-20 02:46:13 +0200

    Code improvement

--- lesson_12/js/main.js
+++ lesson_12/js/main.js
@@ -3,6 +3,7 @@ arr.push(-42);
 arr.unshift(1000);
 arr.push('Last array element');
 arr.splice(arr.indexOf("JavaScript"), 1);
+var filterArr = arr.filter(value => value !== 'JavaScript');
 var numberArr = arr.filter(value => typeof value === 'number');
 var sqareArr = numberArr.map(value => value * value);
 console.log(Math.max.apply(null, sqareArr));
